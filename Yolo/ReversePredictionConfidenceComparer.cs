2680517a9cf947b28c7a801310d8131b5956655d
Katter <narcat99@gmail.com>
2024-09-27 15:27:49 +0500

    ReversePredictionConfidenceComparer code formatting

--- Yolo/ReversePredictionConfidenceComparer.cs
+++ Yolo/ReversePredictionConfidenceComparer.cs
@@ -1,14 +1,17 @@
 namespace Yolo;
 
-internal sealed class ReversePredictionConfidenceComparer : IComparer<Prediction>
+internal sealed class ReversePredictionConfidenceComparer : IComparer<Classification>
 {
 	public static ReversePredictionConfidenceComparer Instance { get; } = new();
 
-	public int Compare(Prediction? x, Prediction? y)
+	public int Compare(Classification? x, Classification? y)
 	{
-		if (ReferenceEquals(x, y)) return 0;
-		if (y is null) return -1;
-		if (x is null) return 1;
+		if (ReferenceEquals(x, y))
+			return 0;
+		if (y is null)
+			return -1;
+		if (x is null)
+			return 1;
 		return y.Confidence.CompareTo(x.Confidence);
 	}
 }
