3aa34daa25324447b2a303754f31e039b38decc5
Katter <narcat99@gmail.com>
2024-10-02 20:07:22 +0500

    Experimental caching in Predictor

--- Yolo/Predictor.cs
+++ Yolo/Predictor.cs
@@ -1,6 +1,5 @@
 using CommunityToolkit.Diagnostics;
 using Microsoft.ML.OnnxRuntime;
-using Microsoft.ML.OnnxRuntime.Tensors;
 using Yolo.OutputProcessing;
 
 namespace Yolo;
@@ -15,8 +14,15 @@ public sealed class Predictor : IDisposable
 		Metadata = new Metadata(_session);
 		if (Metadata.Task == Task.Pose)
 			PoserMetadata = new PoserMetadata(_session);
-		_tensorInfo = new TensorInfo(_session);
-		Guard.IsEqualTo(_tensorInfo.Input.Dimensions[0], Metadata.BatchSize);
+		var tensorInfo = new TensorInfo(_session);
+		Guard.IsEqualTo(tensorInfo.Input.Dimensions[0], Metadata.BatchSize);
+		_ioBinding = _session.CreateIoBinding();
+		_output = RawOutput.Create(_ioBinding, tensorInfo);
+		_inputTensorOwner = DenseTensorOwner<float>.Allocate(tensorInfo.Input);
+		_inputValue = OrtValue.CreateTensorValueFromMemory(
+			OrtMemoryInfo.DefaultInstance,
+			_inputTensorOwner.Tensor.Buffer,
+			tensorInfo.Input.Dimensions64);
 	}
 
 	public IReadOnlyList<TResult> Predict<TPixel, TResult>(
@@ -25,46 +31,41 @@ public sealed class Predictor : IDisposable
 		OutputProcessor<TResult> outputProcessor)
 		where TPixel : unmanaged
 	{
-		using var ioBinding = _session.CreateIoBinding();
-		using var output = RawOutput.Create(ioBinding, _tensorInfo);
-		using var inputTensorOwner = ProcessInput(data, inputProcessor);
-		BindInput(inputTensorOwner.Tensor, ioBinding);
-		_session.RunWithBinding(_runOptions, ioBinding);
-		return outputProcessor.Process(output);
+		ProcessInput(data, inputProcessor);
+		_ioBinding.BindInput(_session.InputNames.Single(), _inputValue);
+		_session.RunWithBinding(_runOptions, _ioBinding);
+		return outputProcessor.Process(_output);
 	}
 
 	public void Dispose()
 	{
+		_inputValue.Dispose();
+		_runOptions.Dispose();
+		_output.Dispose();
+		_inputTensorOwner.Dispose();
+		_ioBinding.Dispose();
 		_session.Dispose();
 	}
 
 	internal PoserMetadata? PoserMetadata { get; }
 
-	private readonly InferenceSession _session;
-	private readonly TensorInfo _tensorInfo;
+	private readonly OrtValue _inputValue;
 	private readonly RunOptions _runOptions = new();
+	private readonly RawOutput _output;
+	private readonly DenseTensorOwner<float> _inputTensorOwner;
+	private readonly OrtIoBinding _ioBinding;
+	private readonly InferenceSession _session;
 
-	private DenseTensorOwner<float> ProcessInput<TPixel>(ReadOnlySpan2D<TPixel> data, InputProcessor<TPixel> inputProcessor) where TPixel : unmanaged
+	private void ProcessInput<TPixel>(ReadOnlySpan2D<TPixel> data, InputProcessor<TPixel> inputProcessor) where TPixel : unmanaged
 	{
-		var inputTensorOwner = DenseTensorOwner<float>.Allocate(_tensorInfo.Input);
 		if (data.Size == Metadata.ImageSize)
-			inputProcessor.ProcessInput(data, inputTensorOwner.Tensor);
+			inputProcessor.ProcessInput(data, _inputTensorOwner.Tensor);
 		else
 		{
 			Span<TPixel> flatResizedData = stackalloc TPixel[Metadata.ImageSize.X * Metadata.ImageSize.Y];
 			Span2D<TPixel> resizedData = new(Metadata.ImageSize, flatResizedData);
 			NearestNeighbourImageResizer.Resize(data, resizedData);
-			inputProcessor.ProcessInput(resizedData, inputTensorOwner.Tensor);
+			inputProcessor.ProcessInput(resizedData, _inputTensorOwner.Tensor);
 		}
-		return inputTensorOwner;
-	}
-
-	private void BindInput(DenseTensor<float> tensor, OrtIoBinding binding)
-	{
-		var inputValue = OrtValue.CreateTensorValueFromMemory(
-			OrtMemoryInfo.DefaultInstance,
-			tensor.Buffer,
-			_tensorInfo.Input.Dimensions64);
-		binding.BindInput(_session.InputNames.Single(), inputValue);
 	}
 }
