3ece2ee5db318ff63d364cdee64da2ee0902112d
Katter <narcat99@gmail.com>
2024-10-02 18:38:44 +0500

    Optimize NearestNeighbourImageResizer

--- Yolo/NearestNeighbourImageResizer.cs
+++ Yolo/NearestNeighbourImageResizer.cs
@@ -5,14 +5,16 @@ internal static class NearestNeighbourImageResizer
 	public static void Resize<TPixel>(ReadOnlySpan2D<TPixel> source, Span2D<TPixel> target)
 	{
 		Vector2D<float> scaleFactor = (Vector2D<float>)source.Size / (Vector2D<float>)target.Size;
-		for (int y = 0; y < target.Size.X; y++)
-		for (int x = 0; x < target.Size.Y; x++)
+		for (int targetYPosition = 0; targetYPosition < target.Size.Y; targetYPosition++)
 		{
-			Vector2D<int> targetPosition = new(x, y);
-			Vector2D<int> sourcePosition = new(
-				(int)Math.Round(targetPosition.X * scaleFactor.X, MidpointRounding.ToZero),
-				(int)Math.Round(targetPosition.Y * scaleFactor.Y, MidpointRounding.ToZero));
-			target[targetPosition] = source[sourcePosition];
+			var sourceYPosition = (int)Math.Round(targetYPosition * scaleFactor.Y, MidpointRounding.ToZero);
+			ReadOnlySpan<TPixel> sourceRow = source.Span.Slice(source.Size.X * sourceYPosition, source.Size.X);
+			Span<TPixel> targetRow = target.Span.Slice(target.Size.X * targetYPosition, target.Size.X);
+			for (int targetXPosition = 0; targetXPosition < target.Size.X; targetXPosition++)
+			{
+				var sourceXPosition = (int)Math.Round(targetXPosition * scaleFactor.X, MidpointRounding.ToZero);
+				targetRow[targetXPosition] = sourceRow[sourceXPosition];
+			}
 		}
 	}
 }
