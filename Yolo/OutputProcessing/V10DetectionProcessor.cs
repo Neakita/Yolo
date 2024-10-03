0a3991e5c72f36c7024d006d24e7548ee4213f46
Katter <narcat99@gmail.com>
2024-10-03 21:56:20 +0500

    Greatly refactor V10DetectionProcessor

--- Yolo/OutputProcessing/V10DetectionProcessor.cs
+++ Yolo/OutputProcessing/V10DetectionProcessor.cs
@@ -3,7 +3,7 @@ using CommunityToolkit.Diagnostics;
 
 namespace Yolo.OutputProcessing;
 
-public sealed class V10DetectionProcessor : BoundedOutputProcessor<Detection>
+public sealed class V10DetectionProcessor : BoundedOutputProcessor<Detection>, IDisposable
 {
 	public float MinimumConfidence
 	{
@@ -28,67 +28,53 @@ public sealed class V10DetectionProcessor : BoundedOutputProcessor<Detection>
 
 	public IReadOnlyList<Detection> Process(RawOutput output)
 	{
+		const int boundingCoordinates = 4;
 		var tensor = output.Output0;
-		var stride1 = tensor.Strides[1];
+		var stride = tensor.Strides[1];
 		var stride2 = tensor.Strides[2];
-
-		var boxesCount = tensor.Dimensions[1];
-		var boxes = new PooledList<Detection>();
+		var detectionsCount = tensor.Dimensions[1];
 		var tensorSpan = tensor.Buffer.Span;
-
-		for (var index = 0; index < boxesCount; index++)
+		PrepareBuffer();
+		for (ushort detectionIndex = 0; detectionIndex < detectionsCount; detectionIndex++)
 		{
-			var boxOffset = index * stride1;
-
-			var confidence = tensorSpan[boxOffset + 4 * stride2];
-
+			var boxOffset = detectionIndex * stride;
+			var confidence = tensorSpan[boxOffset + boundingCoordinates * stride2];
 			if (confidence <= MinimumConfidence)
-			{
 				continue;
-			}
-
-			var classId = (int)tensorSpan[boxOffset + 5 * stride2];
-			var box = ProcessBounding(tensorSpan, index, stride1);
-
-			if (box.Width == 0 || box.Height == 0)
-			{
+			var classId = (ushort)tensorSpan[boxOffset + 5 * stride2];
+			var bounding = ProcessBounding(tensorSpan, detectionIndex, stride);
+			if (bounding.Area == 0)
 				continue;
-			}
-
-			boxes.Add(new Detection(new Classification((ushort)classId, confidence), box, (ushort)index));
+			Classification classification = new(classId, confidence);
+			Detection detection = new(classification, bounding, detectionIndex);
+			_buffer.Add(detection);
 		}
-		
-		boxes.Sort(ReverseDetectionClassificationConfidenceComparer.Instance);
-
-		return _nonMaxSuppressor.Suppress(boxes);
+		return _buffer;
 	}
 
+	public void Dispose()
+	{
+		_buffer.Dispose();
+		_nonMaxSuppressor.Dispose();
+	}
+
+	private readonly PooledList<Detection> _buffer = new();
 	private readonly NonMaxSuppressor _nonMaxSuppressor = new();
 	private readonly Vector2D<int> _imageSize;
 	private float _minimumConfidence = 0.3f;
 
+	private void PrepareBuffer()
+	{
+		_buffer.Clear();
+	}
+
 	private Bounding ProcessBounding(ReadOnlySpan<float> data, int index, int stride)
 	{
-		var boxOffset = index * stride;
-
-		var x = data[boxOffset + 0];
-		var y = data[boxOffset + 1];
-		var w = data[boxOffset + 2];
-		var h = data[boxOffset + 3];
-
-		return new Bounding(x, y, w, h) / _imageSize;
-
-
-		/*var xCenter = data[0 + index];
-		var yCenter = data[1 * stride + index];
-		var width = data[2 * stride + index];
-		var height = data[3 * stride + index];
-
-		var left = xCenter - width / 2;
-		var top = yCenter - height / 2;
-		var right = xCenter + width / 2;
-		var bottom = yCenter + height / 2;
-
-		return new Bounding(left, top, right, bottom) / _imageSize;*/
+		var offset = index * stride;
+		var left = data[offset + 0];
+		var top = data[offset + 1];
+		var right = data[offset + 2];
+		var bottom = data[offset + 3];
+		return new Bounding(left, top, right, bottom) / _imageSize;
 	}
 }
