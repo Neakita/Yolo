6b4d03d01f4f151d69ef2a9a3e12820f459bad1b
Neakita <thekatterdev@gmail.com>
2024-10-07 18:05:41 +0500

    ClassificationTests

--- Yolo.Tests/Helpers/DetectionsOutputHelper.cs
+++ Yolo.Tests/Helpers/DetectionsOutputHelper.cs
@@ -9,4 +9,10 @@ internal static class DetectionsOutputHelper
 		foreach (var detection in detections)
 			outputHelper.WriteLine($"{metadata.ClassesNames[detection.ClassId]}: {detection.Confidence:P1}");
 	}
+
+	public static void WriteClassifications(ITestOutputHelper outputHelper, Metadata metadata, IReadOnlyCollection<Classification> classifications)
+	{
+		foreach (var classification in classifications)
+			outputHelper.WriteLine($"{metadata.ClassesNames[classification.ClassId]}: {classification.Confidence:P1}");
+	}
 }
