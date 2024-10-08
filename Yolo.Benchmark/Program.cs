c8a72ad1cce22fae48c8853545cbd189ca660f03
Neakita <thekatterdev@gmail.com>
2024-10-08 18:17:46 +0500

    Add DotTraceDiagnoser

--- Yolo.Benchmark/Program.cs
+++ Yolo.Benchmark/Program.cs
@@ -1,4 +1,6 @@
 ﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Diagnostics.dotTrace;
+using BenchmarkDotNet.Reports;
 using BenchmarkDotNet.Running;
 
 namespace Yolo.Benchmark;
@@ -7,6 +9,10 @@ internal static class Program
 {
 	private static void Main(string[] args)
 	{
-		BenchmarkRunner.Run<MatchingSizeClassificationBenchmark>(DefaultConfig.Instance.WithOptions(ConfigOptions.DisableOptimizationsValidator), args);
+		var options = DefaultConfig.Instance
+			.WithOptions(ConfigOptions.DisableOptimizationsValidator)
+			.WithSummaryStyle(SummaryStyle.Default.WithMaxParameterColumnWidth(100))
+			.AddDiagnoser(new DotTraceDiagnoser());
+		BenchmarkRunner.Run(typeof(Program).Assembly, options, args);
 	}
 }
