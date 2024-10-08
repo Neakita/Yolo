a267afcbd50b8554a85f57bd5b9fe2f9375a1b02
Neakita <thekatterdev@gmail.com>
2024-10-08 12:15:17 +0500

    Posing benchmark

---
+++ Yolo.Benchmark/MatchingSizePosingBenchmark.cs
@@ -0,0 +1,78 @@
+using BenchmarkDotNet.Attributes;
+using CommunityToolkit.Diagnostics;
+using Microsoft.ML.OnnxRuntime;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using Yolo.ImageSharp;
+using Yolo.OutputProcessing;
+
+namespace Yolo.Benchmark;
+
+public class MatchingSizePosingBenchmark
+{
+	[Params(
+		"yolov8n-pose-uint8.onnx",
+		"yolov8n-pose160fp32.onnx",
+		"yolov8n-pose224fp32.onnx",
+		"yolov8n-pose320fp32.onnx",
+		"yolov8n-pose480fp32.onnx",
+		"yolov8n-pose640fp32.onnx",
+		"yolov8n-pose800fp32.onnx",
+		"yolo11n-pose160fp32.onnx",
+		"yolo11n-pose224fp32.onnx",
+		"yolo11n-pose320fp32.onnx",
+		"yolo11n-pose480fp32.onnx",
+		"yolo11n-pose640fp32.onnx",
+		"yolo11n-pose800fp32.onnx")]
+	public string ModelName { get; set; } = null!;
+
+	[Params("Cpu", "Cuda", "TensorRT")] public string ExecutionProvider { get; set; } = null!;
+
+	[GlobalSetup]
+	public void Setup()
+	{
+		SessionOptions options = new();
+		switch (ExecutionProvider)
+		{
+			case "Cpu":
+				break;
+			case "Cuda":
+				options.AppendExecutionProvider_CUDA();
+				break;
+			case "TensorRT":
+				options.AppendExecutionProvider_Tensorrt();
+				break;
+			default:
+				throw new ArgumentException($"Unknown ExecutionProvider: {ExecutionProvider}");
+		}
+
+		_predictor = new Predictor(File.ReadAllBytes(Path.Combine("Models", ModelName)), options);
+		var size = _predictor.Metadata.ImageSize.X;
+		var imageFileName = $"bus{size}.png";
+		var image = Image.Load<Rgb24>(Path.Combine("Images", imageFileName));
+		Guard.IsTrue(image.DangerousTryGetSinglePixelMemory(out var data));
+		_imageData = data.ToArray();
+		_outputProcessor = new V8PoseProcessor(_predictor);
+		_imageSize = new Vector2D<int>(image.Width, image.Height);
+	}
+
+	[GlobalCleanup]
+	public void CleanUp()
+	{
+		_predictor.Dispose();
+	}
+
+	[Benchmark]
+	public IReadOnlyList<Pose> Predict()
+	{
+		var result = _predictor.Predict(new ReadOnlySpan2D<Rgb24>(_imageSize, _imageData), InputProcessor, _outputProcessor);
+		Guard.IsGreaterThan(result.Count, 0);
+		return result;
+	}
+
+	private static readonly Rgb24InputProcessor InputProcessor = new();
+	private Predictor _predictor = null!;
+	private Rgb24[] _imageData = null!;
+	private Vector2D<int> _imageSize;
+	private OutputProcessor<Pose> _outputProcessor = null!;
+}
