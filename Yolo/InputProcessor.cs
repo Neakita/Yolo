6fdaaa1c32ff63e1d191b5e0c8200b976942bf51
Neakita <thekatterdev@gmail.com>
2024-10-08 19:43:53 +0500

    InputProcessor refactoring

--- Yolo/InputProcessor.cs
+++ Yolo/InputProcessor.cs
@@ -7,16 +7,17 @@ public abstract class InputProcessor<TPixel>
 {
 	public void ProcessInput(ReadOnlySpan2D<TPixel> data, DenseTensor<float> tensor)
 	{
-		var greenChannelStart = tensor.Strides[1] * 1;
-		var blueChannelStart = tensor.Strides[1] * 2;
-		var height = tensor.Strides[2];
+		var colorChannelStride = tensor.Strides[1];
+		var greenChannelStart = colorChannelStride * 1;
+		var blueChannelStart = colorChannelStride * 2;
 		var tensorSpan = tensor.Buffer.Span;
-		var width = tensor.Strides[1] / height;
-		for (ushort y = 0; y < width; y++)
+		var width = tensor.Strides[2];
+		var height = colorChannelStride / width;
+		for (ushort y = 0; y < height; y++)
 		{
 			var sourceRow = data[y];
 			var rowStart = width * y;
-			for (ushort x = 0; x < height; x++)
+			for (ushort x = 0; x < width; x++)
 			{
 				var pixel = sourceRow[x];
 				var index = rowStart + x;
