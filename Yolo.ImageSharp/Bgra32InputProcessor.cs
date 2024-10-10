66e6f59aae009a44f0ccbf3dfbc80b91336b3a3d
Katter <narcat99@gmail.com>
2024-10-11 02:36:29 +0500

    Add Bgra32InputProcessor

---
+++ Yolo.ImageSharp/Bgra32InputProcessor.cs
@@ -0,0 +1,12 @@
+﻿using SixLabors.ImageSharp.PixelFormats;
+
+namespace Yolo.ImageSharp;
+
+public class Bgra32InputProcessor : ImageSharp32InputProcessor<Bgra32>
+{
+	public static Bgra32InputProcessor Instance { get; } = new();
+
+	public Bgra32InputProcessor() : base(2, 1, 0)
+	{
+	}
+}
