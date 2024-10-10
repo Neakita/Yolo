b4f769ae022e62943798ad67dbef0d6376831804
Katter <narcat99@gmail.com>
2024-10-11 02:35:23 +0500

    Extract common code from Argb32InputProcessor to new ImageSharp32InputProcessor

--- Yolo.ImageSharp/Argb32InputProcessor.cs
+++ Yolo.ImageSharp/Argb32InputProcessor.cs
@@ -1,43 +1,13 @@
-using System.Buffers;
-using System.Numerics.Tensors;
-using System.Runtime.InteropServices;
 using SixLabors.ImageSharp.PixelFormats;
 
 namespace Yolo.ImageSharp;
 
-public sealed class Argb32InputProcessor : InputProcessor<Argb32>
+public sealed class Argb32InputProcessor : ImageSharp32InputProcessor<Argb32>
 {
 	public static Argb32InputProcessor Instance { get; } = new();
 
-	protected override void WriteNormalizedPixelValues(
-		ReadOnlySpan<Argb32> pixels,
-		RGBChanneledSpans target)
+	public Argb32InputProcessor() : base(1, 2, 3)
 	{
-		var packedPixels = MemoryMarshal.Cast<Argb32, uint>(pixels);
-		WriteChannelValues(packedPixels, RedChannelMask, RedChannelShiftAmount, target.RedChannel);
-		WriteChannelValues(packedPixels, GreenChannelMask, GreenChannelShiftAmount, target.GreenChannel);
-		WriteChannelValues(packedPixels, BlueChannelMask, BlueChannelShiftAmount, target.BlueChannel);
+		
 	}
-
-	private static void WriteChannelValues(
-		ReadOnlySpan<uint> packedPixels,
-		uint channelMask,
-		int channelShift,
-		Span<float> redChannelTarget)
-	{
-		var bufferArray = ArrayPool<uint>.Shared.Rent(packedPixels.Length);
-		var bufferSpan = bufferArray.AsSpan()[..packedPixels.Length];
-		TensorPrimitives.BitwiseAnd(packedPixels, channelMask, bufferSpan);
-		TensorPrimitives.ShiftRightArithmetic(bufferSpan, channelShift, bufferSpan);
-		TensorPrimitives.ConvertChecked<uint, float>(bufferSpan, redChannelTarget);
-		TensorPrimitives.Divide(redChannelTarget, 255.0f, redChannelTarget);
-		ArrayPool<uint>.Shared.Return(bufferArray);
-	}
-
-	private const uint RedChannelMask = 0x00_00_FF_00;
-	private const uint GreenChannelMask = 0x00_FF_00_00;
-	private const uint BlueChannelMask = 0xFF_00_00_00;
-	private const int RedChannelShiftAmount = sizeof(byte) * 8 * 1;
-	private const int GreenChannelShiftAmount = sizeof(byte) * 8 * 2;
-	private const int BlueChannelShiftAmount = sizeof(byte) * 8 * 3;
 }
---
+++ Yolo.ImageSharp/ImageSharp32InputProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System.Buffers;
+using System.Numerics.Tensors;
+using System.Runtime.InteropServices;
+
+namespace Yolo.ImageSharp;
+
+public abstract class ImageSharp32InputProcessor<TPixel> : InputProcessor<TPixel> where TPixel : unmanaged
+{
+	protected ImageSharp32InputProcessor(byte redChannelPosition, byte greenChannelPosition, byte blueChannelPosition)
+	{
+		_redChannelMask = ComputeMask(redChannelPosition);
+		_greenChannelMask = ComputeMask(greenChannelPosition);
+		_blueChannelMask = ComputeMask(blueChannelPosition);
+		_redChannelShift = ComputeShift(redChannelPosition);
+		_greenChannelShift = ComputeShift(greenChannelPosition);
+		_blueChannelShift = ComputeShift(blueChannelPosition);
+	}
+
+	protected override void WriteNormalizedPixelValues(
+		ReadOnlySpan<TPixel> pixels,
+		RGBChanneledSpans target)
+	{
+		var packedPixels = MemoryMarshal.Cast<TPixel, uint>(pixels);
+		WriteChannelValues(packedPixels, _redChannelMask, _redChannelShift, target.RedChannel);
+		WriteChannelValues(packedPixels, _greenChannelMask, _greenChannelShift, target.GreenChannel);
+		WriteChannelValues(packedPixels, _blueChannelMask, _blueChannelShift, target.BlueChannel);
+	}
+
+	private static void WriteChannelValues(
+		ReadOnlySpan<uint> packedPixels,
+		uint channelMask,
+		int channelShift,
+		Span<float> redChannelTarget)
+	{
+		var bufferArray = ArrayPool<uint>.Shared.Rent(packedPixels.Length);
+		var bufferSpan = bufferArray.AsSpan()[..packedPixels.Length];
+		TensorPrimitives.BitwiseAnd(packedPixels, channelMask, bufferSpan);
+		TensorPrimitives.ShiftRightArithmetic(bufferSpan, channelShift, bufferSpan);
+		TensorPrimitives.ConvertChecked<uint, float>(bufferSpan, redChannelTarget);
+		TensorPrimitives.Divide(redChannelTarget, 255.0f, redChannelTarget);
+		ArrayPool<uint>.Shared.Return(bufferArray);
+	}
+
+	private const uint FirstByteMask = 0xFF;
+	private readonly uint _redChannelMask;
+	private readonly uint _greenChannelMask;
+	private readonly uint _blueChannelMask;
+	private readonly int _redChannelShift;
+	private readonly int _greenChannelShift;
+	private readonly int _blueChannelShift;
+
+	private static uint ComputeMask(byte redChannelPosition)
+	{
+		return FirstByteMask << (8 * redChannelPosition);
+	}
+
+	private static int ComputeShift(byte redChannelPosition)
+	{
+		return sizeof(byte) * 8 * redChannelPosition;
+	}
+}
