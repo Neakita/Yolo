ee49add8910c4ea758a21ce44223d465cb1bc16e
Neakita <thekatterdev@gmail.com>
2024-10-22 13:57:39 +0500

    Precompute targetToSourcePositionLookupTable instead of repetitive computation in every row pass

--- Yolo/NearestNeighbourImageResizer.cs
+++ Yolo/NearestNeighbourImageResizer.cs
@@ -7,6 +7,12 @@ public static class NearestNeighbourImageResizer
 	public static void Resize<TPixel>(ReadOnlySpan2D<TPixel> source, Span2D<TPixel> target)
 	{
 		Vector2D<float> scaleFactor = new((float)source.Width / target.Width, (float)source.Height / target.Height);
+		Span<int> targetToSourcePositionLookupTable = stackalloc int[target.Width];
+		for (int targetXPosition = 0; targetXPosition < target.Width; targetXPosition++)
+		{
+			var sourceXPosition = (int)Math.Round(targetXPosition * scaleFactor.X, MidpointRounding.ToZero);
+			targetToSourcePositionLookupTable[targetXPosition] = sourceXPosition;
+		}
 		for (int targetYPosition = 0; targetYPosition < target.Height; targetYPosition++)
 		{
 			var sourceYPosition = (int)Math.Round(targetYPosition * scaleFactor.Y, MidpointRounding.ToZero);
@@ -14,7 +20,7 @@ public static class NearestNeighbourImageResizer
 			Span<TPixel> targetRow = target.GetRowSpan(targetYPosition);
 			for (int targetXPosition = 0; targetXPosition < target.Width; targetXPosition++)
 			{
-				var sourceXPosition = (int)Math.Round(targetXPosition * scaleFactor.X, MidpointRounding.ToZero);
+				var sourceXPosition = targetToSourcePositionLookupTable[targetXPosition];
 				targetRow[targetXPosition] = sourceRow[sourceXPosition];
 			}
 		}
