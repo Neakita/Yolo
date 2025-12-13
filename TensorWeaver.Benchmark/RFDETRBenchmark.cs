bbd6e8f56f150ab6152e07252c1c25326c281bd8
Katter <thekatterdev@gmail.com>
2025-12-14 03:41:51 +0500

    Fix compile time error in benchmark

--- TensorWeaver.Benchmark/RFDETRBenchmark.cs
+++ TensorWeaver.Benchmark/RFDETRBenchmark.cs
@@ -62,7 +62,7 @@ public class RFDETRBenchmark
 	[Benchmark]
 	public IReadOnlyList<Detection> Predict()
 	{
-		_predictor.SetInput(new ReadOnlySpan2D<Argb32>(_imageData, _imageSize.Y, _imageSize.X), _channeledInputProcessor);
+		_predictor.SetInput(new ReadOnlySpan2D<Argb32>(_imageData, _imageSize.Y, _imageSize.X), ImageSharpInputProcessors.Argb32);
 		_predictor.Predict();
 		var result = _predictor.GetOutput(_outputProcessor);
 		Guard.IsGreaterThan(result.Count, 0);
