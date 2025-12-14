88e0c248fb818ada3104ee20a6fb821d98508e7a
Katter <thekatterdev@gmail.com>
2025-12-14 17:10:04 +0500

    Remove buffer crap from V8Pose3DProcessor

--- TensorWeaver.Yolo/V8Pose3DProcessor.cs
+++ TensorWeaver.Yolo/V8Pose3DProcessor.cs
@@ -1,13 +1,10 @@
-using System.Collections.ObjectModel;
-using Collections.Pooled;
-using CommunityToolkit.Diagnostics;
 using Microsoft.ML.OnnxRuntime;
 using TensorWeaver.OutputData;
 using TensorWeaver.OutputProcessing;
 
 namespace TensorWeaver.Yolo;
 
-public sealed class V8Pose3DProcessor : OutputProcessor<ReadOnlyCollection<Pose3D>>, IDisposable
+public sealed class V8Pose3DProcessor : OutputProcessor<List<Pose3D>>, IDisposable
 {
 	public float MinimumConfidence
 	{
@@ -25,66 +22,39 @@ public sealed class V8Pose3DProcessor : OutputProcessor<ReadOnlyCollection<Pose3
 	{
 		_metadata = YoloMetadata.Parse(session);
 		_poseProcessor = new V8PoseProcessor(session);
-		_poserMetadata = PoserMetadata.Parse(session.ModelMetadata.CustomMetadataMap);
-		Guard.IsEqualTo<byte>(_poserMetadata.KeyPointsDimensions, 3);
-		_posesBuffer = new PooledList<Pose3D>();
-		_wrappedPosesBuffer = new ReadOnlyCollection<Pose3D>(_posesBuffer);
 	}
 
-	public ReadOnlyCollection<Pose3D> Process(RawOutput output)
+	public List<Pose3D> Process(RawOutput output)
 	{
 		const int boundingCoordinatesCount = 4;
 		const int keyPointDimensions = 3;
 		var tensor = output.Tensors[0];
 		var stride = tensor.Strides[1];
 		var poses = _poseProcessor.Process(output);
+		var poses3D = new List<Pose3D>(poses.Count);
 		for (var i = 0; i < poses.Count; i++)
 		{
 			var pose = poses[i];
-			GetKeyPointBuffers(i, out var keyPointsBuffer, out var wrappedKeyPointsBuffer);
+			var keyPoints = new List<KeyPoint3D>(pose.KeyPoints.Count);
 			for (byte keyPointIndex = 0; keyPointIndex < pose.KeyPoints.Count; keyPointIndex++)
 			{
 				var offset = keyPointIndex * keyPointDimensions + boundingCoordinatesCount + _metadata.ClassesNames.Length;
 				var keyPointConfidence = tensor.Buffer.Span[(offset + 2) * stride + pose.Detection.Index];
-				KeyPoint keyPoint = pose.KeyPoints[keyPointIndex];
-				KeyPoint3D keyPoint3D = new(keyPoint.Position, keyPointConfidence);
-				keyPointsBuffer.Add(keyPoint3D);
+				var keyPoint = pose.KeyPoints[keyPointIndex];
+				var keyPoint3D = new KeyPoint3D(keyPoint.Position, keyPointConfidence);
+				keyPoints.Add(keyPoint3D);
 			}
-			Pose3D pose3D = new(pose.Detection, wrappedKeyPointsBuffer);
-			_posesBuffer.Add(pose3D);
+			var pose3D = new Pose3D(pose.Detection, keyPoints);
+			poses3D.Add(pose3D);
 		}
-		return _wrappedPosesBuffer;
+		return poses3D;
 	}
 
 	public void Dispose()
 	{
 		_poseProcessor.Dispose();
-		_posesBuffer.Dispose();
-		_keyPointBuffers.Dispose();
 	}
 
 	private readonly YoloMetadata _metadata;
 	private readonly V8PoseProcessor _poseProcessor;
-	private readonly PooledList<Pose3D> _posesBuffer;
-	private readonly ReadOnlyCollection<Pose3D> _wrappedPosesBuffer;
-	private readonly PooledList<PooledList<KeyPoint3D>> _keyPointBuffers = new();
-	private readonly PooledList<ReadOnlyCollection<KeyPoint3D>> _keyPointWrappedBuffers = new();
-	private readonly PoserMetadata _poserMetadata;
-
-	private void GetKeyPointBuffers(
-		int index,
-		out PooledList<KeyPoint3D> buffer,
-		out ReadOnlyCollection<KeyPoint3D> wrappedBuffer)
-	{
-		if (index == _keyPointBuffers.Count)
-		{
-			buffer = new PooledList<KeyPoint3D>(_poserMetadata.KeyPointsCount);
-			wrappedBuffer = new ReadOnlyCollection<KeyPoint3D>(buffer);
-			_keyPointBuffers.Add(buffer);
-			_keyPointWrappedBuffers.Add(wrappedBuffer);
-		}
-		buffer = _keyPointBuffers[index];
-		wrappedBuffer = _keyPointWrappedBuffers[index];
-		buffer.Clear();
-	}
 }
