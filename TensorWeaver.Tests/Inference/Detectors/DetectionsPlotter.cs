f53993786fb013b815b4a535c40b2a42c9b4ef37
Katter <thekatterdev@gmail.com>
2025-12-15 15:06:44 +0500

    Simplify detector plotting

--- TensorWeaver.Tests/Inference/Detectors/DetectionsPlotter.cs
+++ TensorWeaver.Tests/Inference/Detectors/DetectionsPlotter.cs
@@ -18,38 +18,37 @@ internal sealed class DetectionsPlotter : ResultHandler<IEnumerable<Detection>>
 	public async Task HandleResultAsync(IEnumerable<Detection> detections, CancellationToken cancellationToken)
 	{
 		using var image = await _imageInfo.LoadAsync(cancellationToken);
-		var plottedImage = Plot(image, _classesNames, detections);
+		var plottedImage = Plot(image, detections);
 		var plottedFilePath = Path.Combine("plotted-images", _plottedFileName);
 		Directory.CreateDirectory("plotted-images");
 		await plottedImage.SaveAsync(plottedFilePath, cancellationToken: cancellationToken);
 	}
 
-	private static Image Plot(Image image, IReadOnlyList<string>? classesNames, IEnumerable<Detection> detections)
+	private Image Plot(Image image, IEnumerable<Detection> detections)
 	{
-		return image.Clone(processingContext => Plot(processingContext, classesNames, detections));
+		return image.Clone(processingContext => Plot(processingContext, detections));
 	}
 
-	private static void Plot(
+	private void Plot(
 		IImageProcessingContext processingContext,
-		IReadOnlyList<string>? classesNames,
 		IEnumerable<Detection> detections)
 	{
 		foreach (var detection in detections)
-			Plot(processingContext, classesNames, detection);
+			Plot(processingContext, detection);
 	}
 
 	private readonly ImageInfo _imageInfo;
 	private readonly IReadOnlyList<string> _classesNames;
 	private readonly string _plottedFileName;
 
-	private static void Plot(IImageProcessingContext processingContext, IReadOnlyList<string>? classesNames, Detection detection)
+	private void Plot(IImageProcessingContext processingContext, Detection detection)
 	{
 		var bounding = detection.Bounding;
 		var imageSize = processingContext.GetCurrentSize();
 		bounding *= new Vector2D<int>(imageSize.Width, imageSize.Height);
 		Plot(processingContext, bounding);
 		PointF labelLocation = new(bounding.Left, bounding.Top);
-		Plot(processingContext, classesNames, detection.Classification, labelLocation);
+		Plot(processingContext, detection.Classification, labelLocation);
 	}
 
 	private static void Plot(IImageProcessingContext processingContext, Bounding bounding)
@@ -58,9 +57,9 @@ internal sealed class DetectionsPlotter : ResultHandler<IEnumerable<Detection>>
 		processingContext.Draw(Color.Red, 1, rectangle);
 	}
 
-	private static void Plot(IImageProcessingContext processingContext, IReadOnlyList<string>? classesNames, Classification classification, PointF labelLocation)
+	private void Plot(IImageProcessingContext processingContext, Classification classification, PointF labelLocation)
 	{
-		var label = $"{(object?)classesNames?[classification.ClassId] ?? classification.ClassId}: {classification.Confidence:P1}";
+		var label = $"{_classesNames[classification.ClassId]}: {classification.Confidence:P1}";
 		processingContext.DrawText(label, FontProvider.Font, new Color(new Rgb24(0, 255, 0)), labelLocation);
 	}
 }
