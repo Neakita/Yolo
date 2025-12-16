73016da7e0c6b29041ca549dd3902a75ee8169ce
Katter <thekatterdev@gmail.com>
2025-12-17 00:42:08 +0500

    Add YoloV8ClassificationsProcessor.ClassificationsLimit

--- TensorWeaver.Yolo/YoloV8ClassificationsProcessor.cs
+++ TensorWeaver.Yolo/YoloV8ClassificationsProcessor.cs
@@ -5,17 +5,32 @@ namespace TensorWeaver.Yolo;
 
 public sealed class YoloV8ClassificationsProcessor : OutputProcessor<Classification[]>
 {
+	public int ClassificationsLimit { get; set; } = 5;
+
 	public Classification[] Process(RawOutput output)
 	{
 		var span = output.Tensors[0].Buffer.Span;
-		var classifications = new Classification[span.Length];
-		for (int i = 0; i < span.Length; i++)
+		var classificationsCount = span.Length;
+		Span<float> confidences = stackalloc float[classificationsCount];
+		span.CopyTo(confidences);
+		Span<int> classIds = stackalloc int[classificationsCount];
+		FillWithIndexes(classIds);
+		confidences.Sort(classIds, static (x, y) => y.CompareTo(x));
+		var classificationsCountToReturn = Math.Min(ClassificationsLimit, classificationsCount);
+		var classifications = new Classification[classificationsCountToReturn];
+		for (int i = 0; i < classificationsCountToReturn; i++)
 		{
-			var confidence = span[i];
-			Classification classification = new((ushort)i, confidence);
+			var classId = (ushort)classIds[i];
+			var confidence = confidences[i];
+			var classification = new Classification(classId, confidence);
 			classifications[i] = classification;
 		}
-		classifications.Sort(ReversePredictionConfidenceComparer.Instance);
 		return classifications;
+	}
+
+	private static void FillWithIndexes(Span<int> span)
+	{
+		for (int i = 0; i < span.Length; i++)
+			span[i] = i;
 	}
 }
